ff703f5d8a9f2871d3d56d948f0766dd65ac9491
Julien Crutain <juju510@hotmail.fr>
2023-05-16 11:01:53 +0200

    Configuration de la base de donnée

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'templates/**/*.html.twig',
+    'assets/scripts/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
