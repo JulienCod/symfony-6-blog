9f3d99fcc46f899c5b3a0d304839c7daa2d21320
Julien Crutain <juju510@hotmail.fr>
2023-05-17 18:56:44 +0200

    fix bug login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,33 +2,30 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     
-    #[Route("/login", name:"app_login")]
+    #[Route("/connexion", name:"app_login", methods: ['GET', 'POST'])]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // Récupérer les erreurs d'authentification, le dernier nom d'utilisateur entré
-        // et le dernier message d'erreur (s'il y en a) depuis le service AuthenticationUtils
-        $error = $authenticationUtils->getLastAuthenticationError();
-        $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('welcome/welcome.html.twig', [
-            'last_username' => $lastUsername,
-            'error' => $error,
-        ]);
+    // Récupérer les erreurs d'authentification et le dernier nom d'utilisateur entré
+    $error = $authenticationUtils->getLastAuthenticationError();
+    $lastUsername = $authenticationUtils->getLastUsername();
+
+    return $this->render('welcome/welcome.html.twig', [
+        'last_username' => $lastUsername,
+        'error' => $error,
+    ]);
     }
 
     
-    #[Route("/logout", name:"logout")]
+    #[Route("/deconnexion", name:"app_logout")]
     
     public function logout(): void
     {
